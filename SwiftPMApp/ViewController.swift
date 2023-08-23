f077a1e6927263c13c4992850f9d0b3466c8c861
urassh <shuto.1011@icloud.com>
2023-08-23 21:44:12 +0900

    first commit

---
+++ SwiftPMApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftPMApp
+//
+//  Created by 浦山秀斗 on 2023/08/23.
+//
+
+import UIKit
+import PKHUD
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        HUD.flash(.success, delay: 2.0)
+    }
+
+
+}
+
